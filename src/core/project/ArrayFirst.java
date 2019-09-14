8b2486b6c45849dd205700a450016fc0839924cc
aayush <saayush008@gmail.com>
2019-09-14 22:29:26 +0530

    first commit

---
+++ src/core/project/ArrayFirst.java
@@ -0,0 +1,16 @@
+package core.project;
+
+import java.util.Arrays;
+
+public class ArrayFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] x = new int[] {1,2,3};
+		
+		System.out.print(Arrays.toString(x));
+
+	}
+
+}
